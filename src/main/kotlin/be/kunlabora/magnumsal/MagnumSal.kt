f99e0ad6a1c4d18a7155937dd08495fd6b999566
sch3lp <sch3lp@gmail.com>
2020-02-04 22:06:58 +0100

    Implemented a tiiiiiiny bit more

--- src/main/kotlin/be/kunlabora/magnumsal/MagnumSal.kt
+++ src/main/kotlin/be/kunlabora/magnumsal/MagnumSal.kt
@@ -5,7 +5,18 @@ sealed class MagnumSalEvent: Event {
 }
 
 class MagnumSal(private val eventStream: EventStream) {
+    /**
+     * Adds a player with the given name to a game of MagnumSal.
+     * This is merely an example that works in tandem with the already existing PlayerAdded event.
+     * When you've managed to implement this and get the existing test green,
+     * you should feel confident enough to continue on your own.
+     * If that's not the case, take a look at the hints in the README.md and/or read the assignment (actual exercise) again.
+     */
     fun addPlayer(playerName: String) {
         TODO("add an event to the event stream")
+    }
+
+    fun placeWorker() {
+        TODO("FIRST create a test, then implement this accordingly")
     }
 }
